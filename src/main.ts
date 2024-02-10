69a10da2117572597f671ddb83f5c4c22a53025a
karanmanral18 <karanmanral18@gmail.com>
2024-02-10 21:28:11 +0530

    set env changes

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
