e8c29faa5eaef510dccf06d6bcc3a7b2d0dbe5e3
karanmanral18 <karanmanral18@gmail.com>
2024-02-10 22:17:01 +0530

    make swagger persistent

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,11 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(process.env.PORT, '0.0.0.0');
 }
